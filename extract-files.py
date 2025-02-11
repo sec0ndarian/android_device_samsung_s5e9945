c23025ce410502c6a9565568b2c321e4bab2a319
Secondarian <robertanitei@tutanota.com>
2025-02-11 17:23:35 +0200

    e1s: Initial OpenCL setup

--- extract-files.py
+++ extract-files.py
@@ -46,6 +46,11 @@ blob_fixups: blob_fixups_user_type = {
         .clear_symbol_version('AHardwareBuffer_getId')
         .clear_symbol_version('AHardwareBuffer_getNativeHandle')
         .clear_symbol_version('AHardwareBuffer_release'),
+    'vendor/lib64/libOpenCL.so': blob_fixup()
+        .clear_symbol_version('AHardwareBuffer_acquire')
+        .clear_symbol_version('AHardwareBuffer_describe')
+        .clear_symbol_version('AHardwareBuffer_getNativeHandle')
+        .clear_symbol_version('AHardwareBuffer_release'),
     (
         'vendor/lib64/libalsautils_sec.so',
         'vendor/lib64/libaudioroute_samsung.so',
