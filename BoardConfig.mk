1d2bdf23054fbadbc9cb8ad44825fbb8427d28e8
Secondarian <robertanitei@tutanota.com>
2025-02-12 10:25:51 +0200

    e1s: Initial DRM setup

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -124,6 +124,9 @@ TARGET_USERIMAGES_USE_F2FS := true
 # Releasetools
 TARGET_RELEASETOOLS_EXTENSIONS := device/samsung/e1s
 
+# SELinux
+include device/samsung_slsi/sepolicy/sepolicy.mk
+
 # USB
 $(call soong_config_set,samsungUsbGadgetVars,gadget_name,17900000.dwc3)
 
