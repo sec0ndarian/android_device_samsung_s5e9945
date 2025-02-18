b50b261560ae1144e8c271d31062dc978f2ec0da
Secondarian <robertanitei@tutanota.com>
2025-02-18 22:05:22 +0200

    e1s: Blob composer resources
    * Needed because the following commit made them incompatible with our hwc:
     https://github.com/LineageOS/android_hardware_interfaces/commit/6d0abbb439a8eb7199beeae35e599d6594edbbef

--- extract-files.py
+++ extract-files.py
@@ -24,6 +24,13 @@ namespace_imports = [
 
 
 blob_fixups: blob_fixups_user_type = {
+    'vendor/bin/hw/android.hardware.graphics.composer3-service.exynos': blob_fixup()
+        .replace_needed(
+            'android.hardware.graphics.composer@2.1-resources.so',
+            'android.hardware.graphics.composer@2.1-resources_samsung.so')
+        .replace_needed(
+            'android.hardware.graphics.composer@2.2-resources.so',
+            'android.hardware.graphics.composer@2.2-resources_samsung.so'),
     'vendor/etc/init/init.nfc.samsung.rc': blob_fixup()
         .regex_replace('system', 'secure_element'),
     'vendor/etc/init/init.s5e9945.rc': blob_fixup()
@@ -35,6 +42,10 @@ blob_fixups: blob_fixups_user_type = {
         .regex_replace('.*sec\\.(.|\n)*D', '    </D'),
     'vendor/etc/vintf/manifest/sec_c2_manifest_default0_1_2.xml': blob_fixup()
         .regex_replace('.*t0.*\n', ''),
+    'vendor/lib64/android.hardware.graphics.composer@2.2-resources_samsung.so': blob_fixup()
+        .replace_needed(
+            'android.hardware.graphics.composer@2.1-resources.so',
+            'android.hardware.graphics.composer@2.1-resources_samsung.so'),
     (
         'vendor/lib64/hw/audio.primary.s5e9945.so',
         'vendor/lib64/libaudioproxy2.so',
