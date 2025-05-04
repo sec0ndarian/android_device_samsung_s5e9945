41b40796467b160a75ee0db12bf8392ad11d662e
Secondarian <robertanitei@tutanota.com>
2025-05-04 18:02:26 +0200

    e1s: Switch to Clearkey APEX

--- device.mk
+++ device.mk
@@ -104,7 +104,7 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.camera.raw.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.raw.xml
 
 # DRM
-PRODUCT_PACKAGES += android.hardware.drm-service.clearkey
+PRODUCT_PACKAGES += com.android.hardware.drm.clearkey
 
 # Display
 $(call inherit-product, $(SRC_TARGET_DIR)/product/angle_default.mk)
