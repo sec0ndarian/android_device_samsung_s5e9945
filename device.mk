6d3e9327f60c03ac8890c0fe5c09fc2f46eeeed8
Secondarian <robertanitei@tutanota.com>
2025-05-22 18:14:32 +0200

    e1s: Import audio policy configuration

--- device.mk
+++ device.mk
@@ -46,6 +46,7 @@ PRODUCT_SOONG_NAMESPACES += hardware/samsung
 
 # Audio
 PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_policy_configuration.xml \
     frameworks/av/services/audiopolicy/config/audio_policy_volumes.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_policy_volumes.xml \
     frameworks/av/services/audiopolicy/config/bluetooth_with_le_audio_policy_configuration_7_0.xml:$(TARGET_COPY_OUT_VENDOR)/etc/bluetooth_audio_policy_configuration.xml \
     frameworks/av/services/audiopolicy/config/default_volume_tables.xml:$(TARGET_COPY_OUT_VENDOR)/etc/default_volume_tables.xml \
