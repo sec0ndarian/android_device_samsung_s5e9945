445d9b161b7c49f6fb565842ec87d7036a377748
Secondarian <robertanitei@tutanota.com>
2026-02-25 18:53:53 +0100

    s5e9945: configs: audio: Import inlined mixer_paths.xml
    Standard libaudioroute does not support includes, resulting in missing
    gain when using a non-inlined mixer_paths.xml, the formatting is also
    fixed for cosmetic reasons

--- common.mk
+++ common.mk
@@ -94,7 +94,8 @@ PRODUCT_PACKAGES += \
     audio.r_submix.default \
     audio.usbv2.default \
     audio_effects.xml \
-    audio_policy_configuration.xml
+    audio_policy_configuration.xml \
+    mixer_paths.xml
 
 TARGET_EXCLUDES_AUDIOFX := true
 
