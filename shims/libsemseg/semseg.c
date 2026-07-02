74b48a13469e72ebf0c4d3146c461dba0da602cf
Secondarian <robertanitei@tutanota.com>
2026-07-03 00:50:09 +0200

    s5e9945: Reduce Unnecessary Camera Dependencies

---
+++ shims/libsemseg/semseg.c
@@ -0,0 +1,9 @@
+int ssl_init() {
+    return 1;
+}
+
+void ssl_destroy() {
+}
+
+void ssl_process() {
+}
---
+++ shims/libvpl_dof/vpl_dof.c
@@ -0,0 +1,23 @@
+void createDofWithPath() {
+}
+
+void destroyDof() {
+}
+
+void dofGetIntermediateResult() {
+}
+
+void dofGetVersionStamp() {
+}
+
+void dofHandleFrame() {
+}
+
+void dofIsPreloadFailed() {
+}
+
+void dofPreLoadWithPath() {
+}
+
+void dofUnload() {
+}
