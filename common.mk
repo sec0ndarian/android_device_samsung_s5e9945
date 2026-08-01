b7de0f44cfa4d4c7205ee9d0138b976d4f17a963
Secondarian <robertanitei@tutanota.com>
2026-08-01 16:25:51 +0200

    s5e9945: Stop Using PRODUCT_COPY_FILES for Audio

--- common.mk
+++ common.mk
@@ -75,11 +75,9 @@ PRODUCT_SOONG_NAMESPACES += \
 $(call inherit-product, packages/modules/Virtualization/apex/product_packages.mk)
 
 # Audio
-PRODUCT_COPY_FILES += \
-    frameworks/av/services/audiopolicy/config/audio_policy_volumes.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_policy_volumes.xml \
-    frameworks/av/services/audiopolicy/config/bluetooth_with_le_audio_policy_configuration_7_0.xml:$(TARGET_COPY_OUT_VENDOR)/etc/bluetooth_with_le_audio_policy_configuration_7_0.xml \
-    frameworks/av/services/audiopolicy/config/default_volume_tables.xml:$(TARGET_COPY_OUT_VENDOR)/etc/default_volume_tables.xml \
-    frameworks/av/services/audiopolicy/config/r_submix_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/r_submix_audio_policy_configuration.xml
+$(call soong_config_set_bool,frameworks_av,use_aosp_audio_policy_volumes,true)
+$(call soong_config_set_bool,frameworks_av,use_aosp_default_volume_tables,true)
+$(call soong_config_set_bool,frameworks_av,use_aosp_r_submix_audio_policy_configuration,true)
 
 PRODUCT_PACKAGES += \
     SamsungDAP \
@@ -88,11 +86,15 @@ PRODUCT_PACKAGES += \
     android.hardware.audio@7.1-impl \
     android.hardware.bluetooth.audio-impl \
     android.hardware.soundtrigger@2.3-impl \
+    aosp_audio_policy_volumes.xml \
+    aosp_default_volume_tables.xml \
+    aosp_r_submix_audio_policy_configuration.xml \
     audio.bluetooth.default \
     audio.r_submix.default \
     audio.usbv2.default \
     audio_effects.xml \
-    audio_policy_configuration.xml
+    audio_policy_configuration.xml \
+    bluetooth_with_le_audio_policy_configuration_7_0.xml
 
 TARGET_EXCLUDES_AUDIOFX := true
 
