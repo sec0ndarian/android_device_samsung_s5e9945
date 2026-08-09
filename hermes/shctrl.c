a71406f3ed40b081efe640bf8c5ce4d86bfcfad3
Secondarian <robertanitei@tutanota.com>
2026-08-09 21:55:37 +0200

    s5e9945: Reimplement 'libshctrl.so'
    * IOCTL definitions were taken from the kernel:
      drivers/soc/samsung/exynos-ssp.c

---
+++ hermes/shctrl.c
@@ -0,0 +1,24 @@
+#include <fcntl.h>
+#include <sys/ioctl.h>
+#include <unistd.h>
+
+#define SSP_DEVICE       "/dev/ssp"
+#define SSP_IOCTL_MAGIC  'c'
+#define SSP_IOCTL_INIT   _IO(SSP_IOCTL_MAGIC, 1)
+#define SSP_IOCTL_EXIT   _IOWR(SSP_IOCTL_MAGIC, 2, uint64_t)
+
+int hwvault_ssp_init() {
+    int fd, ret;
+    fd = open(SSP_DEVICE, O_RDONLY);
+    ret = ioctl(fd, SSP_IOCTL_INIT);
+    close(fd);
+    return ret;
+}
+
+int hwvault_ssp_exit() {
+    int fd, ret;
+    fd = open(SSP_DEVICE, O_RDONLY);
+    ret = ioctl(fd, SSP_IOCTL_EXIT);
+    close(fd);
+    return ret;
+}
