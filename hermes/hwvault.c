8fc5bf2a8486ce2049a70c1bf06e62efbc9d2ad5
Secondarian <robertanitei@tutanota.com>
2026-08-13 12:42:47 +0200

    s5e9945: hermes: Minor Optimisations

--- hermes/hwvault.c
+++ hermes/hwvault.c
@@ -5,24 +5,35 @@
 #include <teecl.h>
 #include <unistd.h>
 
-#define HV_TZ_CMD_GET_CRED   0x271f
-#define HWVAULT_CRED_SLOT    11
-#define SHARED_MEM_SIZE      140
-#define SHARED_MEM_SIZE_IN   16
-#define SHARED_MEM_SIZE_OUT  124
-#define TRUSTLET_PATH        "/vendor/tee/00000000-0000-0000-0000-487641557457"
-#define TRUSTLET_ID          "\0\0\0\0\0\0\0\0\0\0HvAUtW"
+#define HV_TZ_CMD_GET_CRED  0x271f
+#define HWVAULT_CRED_SLOT   11
+#define TRUSTLET_PATH       "/vendor/tee/00000000-0000-0000-0000-487641557457"
+#define TRUSTLET_ID         "\0\0\0\0\0\0\0\0\0\0HvAUtW"
 
 void *context, *session;
 
+int payload[] = {
+    HV_TZ_CMD_GET_CRED,
+    8,
+    0x1000005,
+    HWVAULT_CRED_SLOT
+};
+
 struct shared_mem in = {
-    .size = SHARED_MEM_SIZE_IN,
+    .ptr = payload,
+    .size = 16,
     .mode = 1
 };
 
 struct shared_mem out = {
-    .size = SHARED_MEM_SIZE_OUT,
+    .size = 124,
     .mode = 2
+};
+
+struct message msg = {
+    .type = 0xcc00000000,
+    .in = &in,
+    .out = &out
 };
 
 void nwd_tz_open() {
@@ -49,35 +60,16 @@ void nwd_tz_close() {
     TEEC_FinalizeContext(&context);
 }
 
-void nwd_tz_run_cmd() {
-    struct message msg = {
-        .type = 0xcc00000000,
-        .in = &in,
-        .out = &out
-    };
-
-    TEEC_InvokeCommand(&session, 0, &msg, NULL);
-}
-
-void hv_run_cmd() {
-    hwvault_ssp_init();
-    nwd_tz_run_cmd();
-    hwvault_ssp_exit();
-}
-
 void hwvault_get_cred() {
-    in.ptr = malloc(SHARED_MEM_SIZE);
-    TEEC_RegisterSharedMemory(&context, &in);
-    out.ptr = in.ptr + in.size;
+    out.ptr = malloc(out.size);
     TEEC_RegisterSharedMemory(&context, &out);
+    TEEC_RegisterSharedMemory(&context, &in);
 
-    in.ptr[0] = HV_TZ_CMD_GET_CRED;
-    in.ptr[1] = 8;
-    in.ptr[2] = 0x1000005;
-    in.ptr[3] = HWVAULT_CRED_SLOT;
-    hv_run_cmd();
+    hwvault_ssp_init();
+    TEEC_InvokeCommand(&session, 0, &msg, NULL);
+    hwvault_ssp_exit();
 
     TEEC_ReleaseSharedMemory(&in);
     TEEC_ReleaseSharedMemory(&out);
-    free(in.ptr);
+    free(out.ptr);
 }
