bc830c612112577811d60b207f7a5757fcff464b
Secondarian <robertanitei@tutanota.com>
2026-08-24 18:16:09 +0200

    s5e9945: gatekeeper: RE Initialisation

--- gatekeeper/TeegrisGateKeeper.h
+++ gatekeeper/TeegrisGateKeeper.h
@@ -14,15 +14,72 @@
  * limitations under the License.
  *
  */
+#include <fcntl.h>
+#include <sys/stat.h>
+#include <unistd.h>
+
+extern "C" {
+#include <teecl.h>
+}
+
+#define TRUSTLET_PATH  "/vendor/tee/00000000-0000-0000-0000-474154454b45"
+#define TRUSTLET_ID    "\0\0\0\0\0\0\0\0\0\0GATEKE"
 
 namespace gatekeeper {
 
 class TeegrisGateKeeper {
   public:
-    TeegrisGateKeeper() {}
-    ~TeegrisGateKeeper() {}
+    TeegrisGateKeeper() {
+        int fd;
+        size_t size;
+        struct stat st;
+        void* data;
+
+        TEEC_InitializeContext(NULL, &context);
+
+        fd = open(TRUSTLET_PATH, O_RDONLY);
+        fstat(fd, &st);
+        size = st.st_size;
+        data = malloc(size);
+        read(fd, data, size);
+        close(fd);
+
+        TEECS_OpenSession(&context, &session, TRUSTLET_ID, data, size, 0, NULL, NULL, NULL);
+        free(data);
+
+        TEEC_AllocateSharedMemory(&context, &shmem_sz_cmd);
+        TEEC_AllocateSharedMemory(&context, &shmem_sz_pwd_handle);
+        TEEC_AllocateSharedMemory(&context, &shmem_sz_pwd);
+    }
+
+    ~TeegrisGateKeeper() {
+        TEEC_ReleaseSharedMemory(&shmem_sz_cmd);
+        TEEC_ReleaseSharedMemory(&shmem_sz_pwd_handle);
+        TEEC_ReleaseSharedMemory(&shmem_sz_pwd);
+        TEEC_CloseSession(&session);
+        TEEC_FinalizeContext(&context);
+    }
+
     void Enroll(const EnrollRequest &request __unused, EnrollResponse *response __unused) {}
     void Verify(const VerifyRequest &request __unused, VerifyResponse *response __unused) {}
+
+  private:
+    void *context, *session;
+
+    struct shared_mem shmem_sz_cmd = {
+        .size = 0x438,
+        .mode = 3
+    };
+
+    struct shared_mem shmem_sz_pwd_handle = {
+        .size = 0x40,
+        .mode = 1
+    };
+
+    struct shared_mem shmem_sz_pwd = {
+        .size = 0x80,
+        .mode = 1
+    };
 };
 
 }  // namespace gatekeeper
---
+++ gatekeeper/teecl.h
@@ -0,0 +1,13 @@
+struct shared_mem {
+    uint8_t *ptr;
+    size_t size;
+    uint32_t mode;
+    uint64_t reserved;
+};
+
+void TEEC_AllocateSharedMemory(void **, struct shared_mem *);
+void TEEC_CloseSession(void **);
+void TEEC_FinalizeContext(void **);
+void TEEC_InitializeContext(void*, void**);
+void TEEC_ReleaseSharedMemory(struct shared_mem *);
+void TEECS_OpenSession(void **, void **, const char[], void *, size_t, int, void *, void *, int *);
